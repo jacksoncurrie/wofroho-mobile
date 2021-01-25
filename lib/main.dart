1f4074d24bb3411d368f4360a42527abe18a079f
Jackson Currie <jackson.currie@radfords.co.nz>
2021-01-26 11:56:50 +1300

    Remove redundant status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:wofroho_mobile/pages/login_page.dart';
 import 'theme.dart' as MyTheme;
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-    statusBarBrightness: Brightness.light,
-  ));
-
   runApp(MyApp());
 }
 
