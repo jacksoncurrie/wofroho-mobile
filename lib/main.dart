eee717eb4c6c90428e3b0395bf157f8066e8a5e3
Jackson Currie <j.currie1999@gmail.com>
2021-01-26 19:38:21 +1300

    Fix status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wofroho_mobile/pages/login_page.dart';
 import 'theme.dart' as MyTheme;
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+  ));
+
   runApp(MyApp());
 }
 
