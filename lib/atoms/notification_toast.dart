a9442f6f95abee3087c9e773c187e5785a000262
Jackson Currie <j.currie1999@gmail.com>
2021-02-14 11:02:10 +1300

    Make notification ignore pointer events

--- lib/atoms/notification_toast.dart
+++ lib/atoms/notification_toast.dart
@@ -13,29 +13,31 @@ class NotificationToast extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: AnimatedOpacity(
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 400),
-          opacity: isShown ? 1.0 : 0.0,
-          child: Container(
-            decoration: BoxDecoration(
-              color: color,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.25),
-                  spreadRadius: 7,
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
-                ),
-              ],
-              borderRadius: BorderRadius.circular(5),
+    return IgnorePointer(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: AnimatedOpacity(
+            curve: Curves.ease,
+            duration: Duration(milliseconds: 400),
+            opacity: isShown ? 1.0 : 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: color,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.25),
+                    spreadRadius: 7,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: child,
             ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: child,
           ),
         ),
       ),
